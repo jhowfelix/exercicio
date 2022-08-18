25401b240d23954472ed24dba69956511d9c644c
Jonathan Felix <jhow.wi56@gmail.com>
2022-08-18 08:53:38 -0300

    add

---
+++ models/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_orientacao_a_objetos.models
+{
+    public class Cliente
+    {
+        public long Id;
+        public String nome;
+        public String cpf;
+
+        public Cliente(long id, string nome, string cpf)
+        {
+            Id = id;
+            this.nome = nome;
+            this.cpf = cpf;
+        }
+    }
+
+
+
+    
+}
---
+++ models/Conta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_orientacao_a_objetos.models
+{
+    internal abstract class Conta
+    {
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+        public Decimal Saldo { get; set; }
+        public DateTime DataAbertura { get; set; }
+        public IList<Cliente> Clientes { get; set; }
+
+        public Conta(int agencia, int numero, decimal saldo, DateTime dataAbertura, IList<Cliente> clientes)
+        {
+            Agencia = agencia;
+            Numero = numero;
+            Saldo = saldo;
+            DataAbertura = dataAbertura;
+            Clientes = clientes;
+        }
+
+        public abstract void Depositar(decimal valor);
+
+        public abstract void Retirar(decimal valor);
+    }
+}
